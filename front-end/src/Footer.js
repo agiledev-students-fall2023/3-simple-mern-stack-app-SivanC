cfbd7e55a4f597bf646f1ed323ec5e5851f022d2
Sivan Cooperman <sivancooperman@gmail.com>
2023-10-01 13:43:33 -0400

    More rebasing

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -1,6 +1,5 @@
 import './Footer.css'
 
-/**
  * A React component that is used for the footer displayed at the bottom of every page of the site.
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
